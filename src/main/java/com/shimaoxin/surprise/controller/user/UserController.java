60ec1363d2a2976ff063a742d3640676e7c73ab4
shimaoxin <232601982@qq.com>
2018-02-07 10:22:50 +0800

    修改成熟悉的味道

--- src/main/java/com/shimaoxin/surprise/controller/user/UserController.java
+++ src/main/java/com/shimaoxin/surprise/controller/user/UserController.java
@@ -10,9 +10,9 @@ import com.shimaoxin.surprise.service.user.IUserService;
 import com.shimaoxin.surprise.util.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -35,12 +35,10 @@ public class UserController extends BaseController{
 
 
     @RequestMapping("/showUser")
-    @ResponseBody
-    public ModelAndView showUser(ModelAndView mav) {
-        mav.setViewName("index");
+    public String showUser(ModelMap model) {
         User user = this.getCurrentUser();
-        mav.addObject("user", user);
-        return mav;
+        model.put("user", user);
+        return "index";
     }
 
     @RequestMapping("/login")
